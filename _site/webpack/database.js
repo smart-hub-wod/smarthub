18ca13bbadd2cc6d7a0904d0009b29d23a56b725
khloe-r <khloe.ramdhan@outlook.com>
2021-07-11 17:50:25 -0400

    "hopefully fixed login"

---
+++ _site/webpack/database.js
@@ -0,0 +1,31 @@
+import firebase from "firebase/app"
+
+// //const firebase = require(["firebase/app"]);
+// // If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// // import * as firebase from "firebase/app"
+
+// // Add the Firebase products that you want to use
+// // require("firebase/auth");
+// import "firebase/auth";
+// import "firebase/firestore";
+// //import { db } from "./database";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCoQD_u406p8aUjp2Dp5Euo5KlCOKhcFZ4",
+//     authDomain: "smart-hub-wod.firebaseapp.com",
+//     projectId: "smart-hub-wod",
+//     storageBucket: "smart-hub-wod.appspot.com",
+//     xmessagingSenderId: "543083167758",
+//     appId: "1:543083167758:web:6a1df71072c541cebefc5f"
+// };
+  
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+// var db = firebase.firestore();
+
+// // Required for side-effects
+
+// // var db = firebase1.firestore();
+
+// console.log(db.collection("users"))
---
+++ webpack/auth.js
@@ -0,0 +1,55 @@
+(function () {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCoQD_u406p8aUjp2Dp5Euo5KlCOKhcFZ4",
+        authDomain: "smart-hub-wod.firebaseapp.com",
+        projectId: "smart-hub-wod",
+        storageBucket: "smart-hub-wod.appspot.com",
+        xmessagingSenderId: "543083167758",
+        appId: "1:543083167758:web:6a1df71072c541cebefc5f"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+    const txtEmail = document.getElementById('txtEmail')
+    const txtPassword = document.getElementById('txtPassword')
+    const login = document.getElementById('login_button')
+    const signup = document.getElementById('signup_button')
+    const logout = document.getElementById('logout_button')
+
+    signup.addEventListener('click', e=> {
+        //Get email and password
+        const email = txtEmail.value //CHECK FOR REAL EMAIL
+        const pass = txtPassword.value
+        const auth = firebase.auth();
+        //Sign in            
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise
+            .catch(e => console.log(e.message))  
+    })
+    
+    login.addEventListener('click', e => {
+        //Get email and password
+        const email = txtEmail.value
+        const pass = txtPassword.value
+        const auth = firebase.auth();
+        //Sign in            
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message))  
+    })
+
+    logout.addEventListener('click', e => {
+        firebase.auth().signOut();
+    })
+
+    //Add a realtime listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log(firebaseUser)
+            logout.classList.remove('invisible')
+        }
+        else{
+            console.log("not logged in")
+            logout.classList.add('invisible')
+        }
+    })
+        
+}());
